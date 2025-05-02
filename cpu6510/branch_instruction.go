afd4247c5e9d4fcd0b4cc71c0fab5f7134adedd8
Stefan Alfbo <stefan@alfbo.se>
2025-05-02 11:57:26 +0200

    Refactor the branch instructions to use the same logic since the differences in the instructions are based on which status register flag to look at

--- cpu6510/branch_instruction.go
+++ cpu6510/branch_instruction.go
@@ -1,81 +1,57 @@
 package cpu6510
 
+func branchOnFlag(c *CPU, flag bool) {
+	c.IncrementProgramCounter()
+
+	if flag {
+		c.SetProgramCounter(uint16(int16(c.ProgramCounter()) + int16(c.ReadRam(c.ProgramCounter()))))
+	}
+}
+
 // BPL - Branch if PLus. BPL branches to the given address if the negative
 // flag in the status register is not set.
 func BPL(c *CPU) {
-	c.programCounter++
-
-	if !c.statusRegister.negativeFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, !c.StatusRegister().negativeFlag)
 }
 
 // BMI - Branch if Minus. BMI branches to the given address if the negative
 // flag in the status register is set.
 func BMI(c *CPU) {
-	c.programCounter++
-
-	if c.statusRegister.negativeFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, c.StatusRegister().negativeFlag)
 }
 
 // BVC - Branch if oVerflow Clear. BVC branches to the given address if the
 // overflow flag in the status register is not set.
 func BVC(c *CPU) {
-	c.programCounter++
-
-	if !c.statusRegister.overflowFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, !c.StatusRegister().overflowFlag)
 }
 
 // BVS - Branch if oVerflow Set. BVS branches to the given address if the
 // overflow flag in the status register is set.
 func BVS(c *CPU) {
-	c.programCounter++
-
-	if c.statusRegister.overflowFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, c.StatusRegister().overflowFlag)
 }
 
 // BCC - Branch if Carry Clear. BCC branches to the given address if the
 // carry flag in the status register is not set.
 func BCC(c *CPU) {
-	c.programCounter++
-
-	if !c.statusRegister.carryFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, !c.StatusRegister().carryFlag)
 }
 
 // BCS - Branch if Carry Set. BCS branches to the given address if the
 // carry flag in the status register is set.
 func BCS(c *CPU) {
-	c.programCounter++
-
-	if c.statusRegister.carryFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, c.StatusRegister().carryFlag)
 }
 
 // BNE - Branch if Not Equal. BNE branches to the given address if the
 // zero flag in the status register is not set.
 func BNE(c *CPU) {
-	c.programCounter++
-
-	if !c.statusRegister.zeroFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, !c.StatusRegister().zeroFlag)
 }
 
 // BEQ - Branch if Equal. BEQ branches to the given address if the zero
 // flag in the status register is set.
 func BEQ(c *CPU) {
-	c.programCounter++
-
-	if c.statusRegister.zeroFlag {
-		c.programCounter = uint16(int16(c.programCounter) + int16(c.ram[c.programCounter]))
-	}
+	branchOnFlag(c, c.StatusRegister().zeroFlag)
 }
